40342f071036b407e6c83adb18a059d0e40d1dad
KaaZMi <mikael.14523@gmail.com>
2016-03-31 17:38:15 +0200

    Début du multi-thread.

--- src/view/Fenetre.java
+++ src/view/Fenetre.java
@@ -27,17 +27,18 @@ import javax.swing.JToolBar;
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.ui.swingViewer.*;
-import org.graphstream.ui.view.View;
 import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.ViewerListener;
 import org.graphstream.ui.view.ViewerPipe;
 
 import controler.Controler;
 import model.Model;
 
 @SuppressWarnings("serial")
-public class Fenetre extends JFrame implements Observer {
-	
-	//TEST TEST
+public class Fenetre extends JFrame implements Observer, ViewerListener {
+	protected boolean loop = true;
+	protected ViewerPipe fromViewer = null;
+
 	private JMenuBar menu = null;
 	private JMenu fichier = null;
 	private JMenuItem ouvrirXML = null;
@@ -51,19 +52,19 @@ public class Fenetre extends JFrame implements Observer {
 
 	private Controler controler;
 	private Model model;
-	
+
 	private Graph graph = null;
 	private LinkedHashMap<Integer, ArrayList<String>> events = new LinkedHashMap<Integer, ArrayList<String>>();
 
 	public Fenetre(Controler controler, Model model){
 		this.controler = controler;
 		this.model = model;
-		this.model.addObserver(this); // ajout de la fenêtre comme observer du modèle
-		
+		this.model.addObserver(this); // adding the window as an observer of the model
+
 		initFrame();
 		initMenuBar();
 		initToolBar();
-		
+
 		setVisible(true);
 	}
 
@@ -71,13 +72,13 @@ public class Fenetre extends JFrame implements Observer {
 		setTitle("Smile Graph");
 		setSize(new Dimension(1600,900));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLocationRelativeTo(null); // fenêtre centrée
+		setLocationRelativeTo(null); // window at the center
 		setResizable(true);
-		conteneur.setBackground(Color.red); // temporaire
-		setContentPane(this.conteneur); // avertir notre JFrame que ce sera notre JPanel qui constituera son content pane
-		getContentPane().setLayout(new BorderLayout()); // choix du gestionnaire d'agencement
+		conteneur.setBackground(Color.red); // DEV
+		setContentPane(this.conteneur); // warn our JFrame that the JPanel will constitute its content pane
+		getContentPane().setLayout(new BorderLayout()); // choosing the layout manager
 	}
-	
+
 	private void initMenuBar() {
 		menu = new JMenuBar();
 
@@ -95,8 +96,7 @@ public class Fenetre extends JFrame implements Observer {
 				conteneur.revalidate();
 			}	    	
 		});
-		
-		//TODO: voir pourquoi le jfilechooser ne fonctionne pas ici
+
 		ouvrirLOG = new JMenuItem("Ouvrir scénario");
 		ouvrirLOG.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0){
@@ -113,7 +113,7 @@ public class Fenetre extends JFrame implements Observer {
 		enregistrer.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0){
 				conteneur.removeAll();
-				//conteneur.add(new ScorePanel(size, model.getScores()).getPanel(), BorderLayout.CENTER);
+				System.out.println("Enregistrer");
 				conteneur.revalidate();
 			}
 		});
@@ -150,10 +150,10 @@ public class Fenetre extends JFrame implements Observer {
 		menu.add(fichier);
 		menu.add(help);
 	}
-	
+
 	private void initToolBar() {
 		JToolBar toolbar = new JToolBar();
-		
+
 		JButton prevButton = new JButton(new ImageIcon("playback_prev_icon&16.png"));
 		prevButton.setFocusPainted(false);
 		toolbar.add(prevButton);
@@ -162,34 +162,42 @@ public class Fenetre extends JFrame implements Observer {
 				System.out.println("PREV");
 			}
 		});
-		
+
 		JButton playButton = new JButton(new ImageIcon("playback_play_icon&16.png"));
 		toolbar.add(playButton);
 		playButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				graph.addAttribute("ui.stylesheet", "edge.marked { fill-color: red; }");
-				for (Entry<Integer, ArrayList<String>> entry : events.entrySet()) {
-				    System.out.println(entry.getKey() + "/" + entry.getValue());
-				    String i = entry.getValue().get(1);
-				    String j = entry.getValue().get(2);
-				    String id = "";
-				    if (Integer.parseInt(i) < Integer.parseInt(j)) {
-						id = i+"-"+j+"-"+j+"-"+i;
+				// scenario execution in a separate thread
+				new Thread() {
+					public void run() {
+						for (Entry<Integer, ArrayList<String>> entry : events.entrySet()) {
+							// We need to call the pump() method before each use 
+							// of the graph to copy back events that have already 
+							// occurred in the viewer thread inside our thread.
+							fromViewer.pump();
+							System.out.println(entry.getKey() + "/" + entry.getValue());
+							String i = entry.getValue().get(1);
+							String j = entry.getValue().get(2);
+							String id = "";
+							if (Integer.parseInt(i) < Integer.parseInt(j)) {
+								id = i+"-"+j+"-"+j+"-"+i;
+							}
+							else {
+								id = j+"-"+i+"-"+i+"-"+j;
+							}
+							System.out.println(id);
+							graph.getEdge(id).setAttribute("ui.class", "marked");
+							try {
+								Thread.sleep(1000);
+							} catch (InterruptedException e1) {
+								e1.printStackTrace();
+							}
+						}
 					}
-					else {
-						id = j+"-"+i+"-"+i+"-"+j;
-					}
-				    System.out.println(id);
-				    graph.getEdge(id).setAttribute("ui.class", "marked");
-				    try {
-						Thread.sleep(50);
-					} catch (InterruptedException e1) {
-						e1.printStackTrace();
-					}
-				}
+				}.start();
 			}
 		});
-		
+
 		JButton nextButton = new JButton(new ImageIcon("playback_next_icon&16.png"));
 		toolbar.add(nextButton);
 		nextButton.addActionListener(new ActionListener() {
@@ -197,7 +205,7 @@ public class Fenetre extends JFrame implements Observer {
 				System.out.println("NEXT");
 			}
 		});
-		
+
 		JButton stopButton = new JButton(new ImageIcon("playback_stop_icon&16.png"));
 		toolbar.add(stopButton);
 		stopButton.addActionListener(new ActionListener() {
@@ -205,46 +213,79 @@ public class Fenetre extends JFrame implements Observer {
 				System.out.println("STOP");
 			}
 		});
-		
+
 		toolbar.setFloatable(false);
-		
-		this.setJMenuBar(menu); // menu propre au JFrame
-		this.conteneur.add(toolbar, BorderLayout.NORTH); // menu propre au content pane
+
+		this.setJMenuBar(menu); // add the menu to the JFrame
+		conteneur.add(toolbar, BorderLayout.NORTH); // add the toolbar to the main panel
 	}
 
 	@Override
 	public void update(Observable obs, Object obj) {
-		
-		// initialisation et affichage du graphe original dans la fenetre
+
+		// initialization and displaying the original graph in the window
 		if (graph == null) {
 			graph = new MultiGraph("embedded");
 			graph.setStrict(false);
-			graph.setAutoCreate(true);
-			graph.addAttribute("ui.antialias");
+			graph.setAutoCreate(true); // automatic creation of nodes depending edges
+			graph.addAttribute("ui.antialias"); // graphics smoothing
+			graph.addAttribute("ui.quality");
+			graph.addAttribute("ui.stylesheet", styleSheet); // CSS style of the graph
+
 			LinkedHashMap<String, ArrayList<String>> edges = model.getEdges();
 			for (Entry<String, ArrayList<String>> entry : edges.entrySet()) {
-				graph.addEdge(entry.getKey(), entry.getValue().get(0), entry.getValue().get(1), false); // false = non orienté
+				graph.addEdge(entry.getKey(), entry.getValue().get(0), entry.getValue().get(1), false); // false = not oriented
 			}
 			for (Node node : graph) {
 				node.addAttribute("ui.label", node.getId());
 			}
+
 			Viewer viewer = new Viewer(graph, Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
 			viewer.enableAutoLayout();
-		    ViewPanel view = viewer.addDefaultView(false); // false indicates "no JFrame"
-		    
-		    // TODO : MouseWheelListener pour le zoom
-		    JSlider slider = new JSlider();
-		    slider.addChangeListener(e -> view.getCamera().setViewPercent(slider.getValue() / 10.0));
-		    slider.setBackground(Color.white);
-		    conteneur.add((Component) view, BorderLayout.CENTER);
-		    conteneur.add(slider, BorderLayout.SOUTH);
+			ViewPanel view = viewer.addDefaultView(false); // false indicates "no JFrame"
+
+			// IMPORTANT !
+			// We connect back the viewer to the graph,
+			// the graph becomes a sink for the viewer.
+			// We also install us as a viewer listener to
+			// intercept the graphic events.
+			fromViewer = viewer.newViewerPipe();
+			fromViewer.addViewerListener(this);
+			fromViewer.addSink(graph);
+
+			// TODO : MouseWheelListener pour le zoom au lieu de JSlider (priorité minimal)
+			JSlider slider = new JSlider();
+			slider.addChangeListener(e -> view.getCamera().setViewPercent(slider.getValue() / 10.0));
+			slider.setBackground(Color.white);
+			conteneur.add((Component) view, BorderLayout.CENTER);
+			conteneur.add(slider, BorderLayout.SOUTH);
 		}
-		
+
 		else {
 			events = model.getEvents();
 			System.out.println(events.size());
 		}
-		
+
 	}
+
+	public void viewClosed(String id) {
+		loop = false;
+	}
+
+	public void buttonPushed(String id) {
+		System.out.println("Noeud " + id + " attrapé");
+	}
+
+	public void buttonReleased(String id) {
+		System.out.println("Noeud " + id + " relâché");
+	}
+	
+	// TODO : mettre au point le style (juste un test pour l'instant)
+	// TODO : il faudra mettre ça au propre dans un fichier peut-être
+	protected static String styleSheet =
+			"edge.marked {"+
+			"	fill-color: red;"+
+			"}"
+	;
 
 }
